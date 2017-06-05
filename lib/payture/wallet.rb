edec3beccb173adf87d2bdacc1623609839c3692
cyrille <cderche@users.noreply.github.com>
2017-06-05 16:39:23 +0300

    Added pay function

--- lib/payture/wallet.rb
+++ lib/payture/wallet.rb
@@ -1,9 +1,14 @@
 class Payture::Wallet < Payture::API
 
-  PATH_INIT   = '/vwapi/Init'
+  PATH_INIT = '/vwapi/Init'
+  PATH_PAY  = '/vwapi/Pay'
 
   def init(vwid, data)
     self.request(PATH_INIT, vwid, data)
+  end
+  
+  def pay(vwid, data)
+    self.request(PATH_PAY, vwid, data)
   end
 
 end
