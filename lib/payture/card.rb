20435df22dc702b2437b96fa93b0b481b407cfd3
Kirill <c.derche@me.com>
2017-08-05 08:36:44 +0300

    added ability to remove card

--- lib/payture/card.rb
+++ lib/payture/card.rb
@@ -1,9 +1,14 @@
 class Payture::Card < Payture::Wallet
 
   PATH_LIST = '/vwapi/GetList'
+  PATH_REMOVE = '/vwapi/Remove'
 
   def list(vwid, data)
     self.request(PATH_LIST, vwid, data)
+  end
+
+  def remove(vwid, data)
+    self.request(PATH_REMOVE, vwid, data)
   end
 
 end
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -24,3 +24,18 @@ RSpec.describe Payture::Card, "#list" do
     end
   end
 end
+
+# RSpec.describe Payture::Card, "#remove" do
+#   context "Given a merchant and data" do
+#     it "should get a response with data" do
+#       wallet = Payture::Card.new(HOST)
+#       res = wallet.list(MERCHANT_ADD, DATA_LIST)
+#       puts res
+#       expect(res).not_to be_nil
+#       expect(res['GetList']).not_to be_nil
+#       expect(res['GetList']['VWUserLgn']).not_to be_nil
+#       expect(res['GetList']['Success']).to eq('True')
+#       expect(res['GetList']['Item']).not_to be_nil
+#     end
+#   end
+# end
